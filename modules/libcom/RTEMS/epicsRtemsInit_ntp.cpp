d606ad964a88a38b94f33ff906a4743a64517f91
Chris Johns <chrisj@rtems.org>
2026-06-01 13:25:46 +1000

    RTEMS Initialise: new component initialise in C++
    To enable add to you have local CONFIG_SITE or RELEASE:
    
    RTEMS_INIT = new

---
+++ modules/libcom/RTEMS/epicsRtemsInit_ntp.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************\
+* Copyright (c) 2026 Chris Johns
+* SPDX-License-Identifier: EPICS
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
+
+#include <iostream>
+
+#include <string.h>
+
+#include <epicsRtemsInit.h>
+
+char rtemsInit_NTP_server_ip[16] = "";
+
+static int rtemsNTPInitialize() {
+    auto envp = getenv("RTEMS_NET_NTP_IP");
+    if (envp != nullptr) {
+        ::strlcpy(
+            rtemsInit_NTP_server_ip, envp, sizeof(rtemsInit_NTP_server_ip));
+        std::cout << "NTP IP address: " << rtemsInit_NTP_server_ip
+                  << std::endl;
+    }
+    return 0;
+}
+
+void epicRtemsInit_ntp() {
+    epicsRtemsInitRegisterHandler(
+        "system", "ntp.ip", 600, rtemsNTPInitialize);
+}
